6cab67eec1ba9af5434f440adfb94e2f0d15bf98
Fatima <karimifatima@gmail.com>
2021-12-28 16:39:04 +0330

    admin new controller to manage staffpics

---
+++ BookStore/Areas/Admin/Controllers/SelectionController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BookStore.IRepository;
+using BookStore.Models;
+using BookStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [RouteArea("Admin", AreaPrefix = "Admin")]
+    [RoutePrefix("Selection")]
+    public class SelectionController : Controller
+    {
+        private readonly IGenericRepository<StaffPic> _staffPic;
+        private readonly IMapper _mapper;
+
+        public SelectionController(IGenericRepository<StaffPic> staffPics, IMapper mapper)
+        {
+            _staffPic = staffPics;
+            _mapper = mapper;
+        }
+
+        [Route("List")]
+        public async Task<ActionResult> List()
+        {
+            var staffPics = await _staffPic.GetAllAsync();
+            var results = _mapper.Map<IList<StaffPicViewModel>>(staffPics);
+            return View(results);
+        }
+    }
+}
